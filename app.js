308eae8b826620d7f57b8e4d9f7fb371929b8c11
Gilbert Carrasco <tonytrox@gmail.com>
2025-02-15 23:53:31 -0500

    Connecting to MongoDB

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
+const express = require("express");
+const usersRouter = require("./routes/users");
+const cardsRouter = require("./routes/cards");
+const mongoose = require("mongoose");
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
+mongoose.connect("mongodb://localhost:27017/aroundb", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use("/users", usersRouter);
+app.use("/cards", cardsRouter);
 
 app.use((req, res) => {
   res.status(404).send({ message: "Recurso solicitado no encontrado" });
