43255c9ebc835e0da3048fc88e0361a2a6febbf4
Gilbert Carrasco <tonytrox@gmail.com>
2025-02-24 13:17:27 -0500

    add routes to users & cards

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 const router = require("express").Router(); // const router = express.Router();
 
-const { getUsers, getUserById, createUser } = require("../controllers/users");
+const {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUserProfile,
+  updateUserAvatar,
+} = require("../controllers/users");
 
 router.get("/", getUsers); // Cuando se solicita /, se invoca getUsers
 router.get("/:userId", getUserById);
 router.post("/", createUser);
+router.patch("/me", updateUserProfile);
+router.patch("/me/avatar", updateUserAvatar);
 
 module.exports = router;
